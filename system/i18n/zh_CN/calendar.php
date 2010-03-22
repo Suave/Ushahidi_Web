bc50ab09befc5053f15c9d5622c69510dbb2e63d
makestory <makestory@zoho.com>
2010-03-22 18:49:13 +0800

    trans-makestory ver0.6

---
+++ system/i18n/zh_CN/calendar.php
@@ -0,0 +1,59 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+$lang = array
+(
+	// Two letter days
+	'su'        => '日',
+	'mo'        => '一',
+	'tu'        => '二',
+	'we'        => '三',
+	'th'        => '四',
+	'fr'        => '五',
+	'sa'        => '六',
+
+	// Short day names
+	'sun'       => '日',
+	'mon'       => '一',
+	'tue'       => '二',
+	'wed'       => '三',
+	'thu'       => '四',
+	'fri'       => '五',
+	'sat'       => '六',
+
+	// Long day names
+	'sunday'    => '星期日',
+	'monday'    => '星期一',
+	'tuesday'   => '星期二',
+	'wednesday' => '星期三',
+	'thursday'  => '星期四',
+	'friday'    => '星期五',
+	'saturday'  => '星期六',
+
+	// Short month names
+	'jan'       => '一月',
+	'feb'       => '二月',
+	'mar'       => '三月',
+	'apr'       => '四月',
+	'may'       => '五月',
+	'jun'       => '六月',
+	'jul'       => '七月',
+	'aug'       => '八月',
+	'sep'       => '九月',
+	'oct'       => '十月',
+	'nov'       => '十一月',
+	'dec'       => '十二月',
+
+	// Long month names
+	'january'   => '一月',
+	'february'  => '二月',
+	'march'     => '三月',
+	'april'     => '四月',
+	'mayl'      => '五月',
+	'june'      => '六月',
+	'july'      => '七月',
+	'august'    => '八月',
+	'september' => '九月',
+	'october'   => '十月',
+	'november'  => '十一月',
+	'december'  => '十二月'
+);
