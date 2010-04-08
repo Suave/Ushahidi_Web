8c4c8b94b0aaf2fdd292f17261d81698184cf5a2
makestory <makestory@zoho.com>
2010-04-09 00:49:45 +0800

    文字说明

--- application/views/reports_view.php
+++ application/views/reports_view.php
@@ -71,6 +71,9 @@
 										<li><img align="absmiddle" alt="Incident" src="<?php echo url::base(); ?>media/img/incident-pointer.jpg"/> 发生位置</li>
 										<li><img align="absmiddle" alt="Nearby Incident" src="<?php echo url::base(); ?>media/img/nearby-incident-pointer.jpg"/> 附近其他事件</li>
 									</ul>
+									<br/>
+                                                                <div style="background:#228;float: right;padding:1px 5px;color:white;margin-top:2px">下面的 “+” 按钮可以切换图层</div>
+								</div>
 								</div>
 								<div class="report-map">
 									<div class="map-holder" id="map"></div>
